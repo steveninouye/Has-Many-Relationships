55d18ffc5617389a92557b8c424a597b42e0b7ee
Steven Inouye <steveninouye@msn.com>
2018-04-02 02:44:35 -1000

    finished exercise

--- joins.sql
+++ joins.sql
@@ -23,7 +23,7 @@ SELECT posts.title, posts.content, users.username
 FROM posts INNER JOIN users ON users.user_id = posts.user_id
 WHERE EXTRACT(year FROM posts.created_at) >= 2015;
 -- 1. Create a query to get the all rows in the `comments` table, showing post title (aliased as 'Post Title'), and the all the comment's fields 19729
-SELECT posts.title AS 'Post Title', comments.*
+SELECT posts.title AS "Post Title", comments.*
 FROM comments INNER JOIN posts ON posts.post_id = comments.post_id;
 -- 1. Create a query to get the all rows in the `comments` table, showing post title (aliased as post_title), post url (ailased as post_url), and the comment body (aliased as comment_body) where the post was created before January 1, 2015
 SELECT posts.title AS post_title, posts.url AS post_url, comments.body AS comment_body
@@ -48,12 +48,17 @@ WHERE comments.body LIKE '%SSL%' AND posts.content LIKE '%dolorum%';
 -- 1. Create a query to get the first name of the author of the post (aliased to post_author_first_name), last name of the author of the post (aliased to post_author_last_name), the post title (aliased to post_title), username of the author of the comment (aliased to comment_author_username), and comment body (aliased to comment_body), where the comment body contains the word 'SSL' or 'firewall' and the post content contains the word 'nemo' ( should have 218 results )
 SELECT users.first_name AS post_author_first_name, users.last_name AS post_author_last_name, posts.title AS post_title, users.username AS comment_author_username, comments.body AS comment_body
 FROM comments INNER JOIN posts ON posts.post_id = comments.post_id INNER JOIN users ON users.user_id = comments.user_id
-WHERE comments.body LIKE '%SSL%' OR comments.body LIKE '%firewall%' AND posts.content LIKE '%nemo%';
-
-
+WHERE  comments.body LIKE '%SSL%' OR comments.body LIKE '%firewall%' AND posts.content LIKE '%nemo%';
+SELECT users.first_name AS post_author_first_name, users.last_name AS post_author_last_name, posts.title AS post_title, users.username AS comment_author_username, comments.body AS comment_body
+FROM comments INNER JOIN posts ON posts.post_id = comments.post_id INNER JOIN users ON users.user_id = comments.user_id
+WHERE  comments.body LIKE '%SSL%' || '%firewall%' AND posts.content LIKE '%nemo%';
 -- ### Additional Queries
-
 -- If you finish early, perform and record the following SQL statements in `joins.sql` using these higher level requirements.
-
 -- 1. Count how many comments have been written on posts that have been created after July 14, 2015 ( should have one result, the value of the count should be 27)
+SELECT count(*) AS number_of_comments
+FROM comments
+WHERE EXTRACT(year from created_at) >= 2015 AND EXTRACT(month from created_at) >= 7 AND EXTRACT(day from created_at) >= 14;
 -- 1. Find all users who comment about 'programming' ( should have 336 results)
+SELECT users.first_name AS user_first_name, users.last_name AS user_last_name, users.username AS username
+FROM comments INNER JOIN users ON comments.user_id = users.user_id
+WHERE comments.body LIKE '%programming%';
